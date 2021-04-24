e8daa936b6674e6417d6eb73d2279823715a3806
Kornraphat Jaruphatpukdee <81676386+kornraphat2541@users.noreply.github.com>
2021-04-24 22:25:55 +0700

    Delete djangomail directory

--- djangomail/gmaill/pagemail/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagemailConfig(AppConfig):
-    name = 'pagemail'
--- djangomail/gmaill/pagemail/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-def todoappView(request):
-    return render(request,'index.html')
-# Create your views here.
